3f0c9f29cbc37624b0c2c97ec2663a48d3ddf27c
Vinícius Quatrin <104473428+ViniciusQuatrin@users.noreply.github.com>
2023-07-16 13:37:08 -0300

    adicionado o seeding databse

--- backend/src/main/java/com/projeto/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projeto/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projeto.catalog.entities.Category;
 import com.projeto.catalog.repositories.CategoryRepository;
@@ -14,7 +15,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() { 
 		return repository.findAll();
 	}
+	
+	
 }

695aaa086bac562ae097d8b50d67451ff5ab5b19
Vinícius Quatrin <104473428+ViniciusQuatrin@users.noreply.github.com>
2023-07-17 22:58:48 -0300

    find category by id, exception handle

--- backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
@@ -3,8 +3,8 @@ package com.projeto.catalog.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +14,19 @@ import com.projeto.catalog.services.CategoryService;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryService service;
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll() {
+	public List<CategoryDTO> findAll() {
 		List<CategoryDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		return list;
+	}
+
+	@GetMapping(value = "/{Id}")
+	public CategoryDTO findById(@PathVariable Long Id) {
+		CategoryDTO dto = service.findById(Id);
+		return dto;
 	}
 }
--- backend/src/main/java/com/projeto/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projeto/catalog/services/CategoryService.java
@@ -9,18 +9,24 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projeto.catalog.dto.CategoryDTO;
 import com.projeto.catalog.entities.Category;
 import com.projeto.catalog.repositories.CategoryRepository;
+import com.projeto.catalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() { 
+	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).toList();
 	}
-	
-	
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long Id) {
+		Category cat = repository.findById(Id).orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(cat);
+	}
+
 }

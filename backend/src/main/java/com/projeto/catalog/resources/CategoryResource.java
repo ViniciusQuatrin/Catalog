8392346add337d3c7367b935d916da42d4e78f27
Vinícius Quatrin <104473428+ViniciusQuatrin@users.noreply.github.com>
2023-07-17 23:17:00 -0300

    POST new Category criado

--- backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/projeto/catalog/resources/CategoryResource.java
@@ -1,12 +1,17 @@
 package com.projeto.catalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.catalog.dto.CategoryDTO;
 import com.projeto.catalog.services.CategoryService;
@@ -28,5 +33,12 @@ public class CategoryResource {
 	public CategoryDTO findById(@PathVariable Long Id) {
 		CategoryDTO dto = service.findById(Id);
 		return dto;
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) { 
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{Id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/projeto/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projeto/catalog/services/CategoryService.java
@@ -29,4 +29,12 @@ public class CategoryService {
 		return new CategoryDTO(cat);
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
 }
